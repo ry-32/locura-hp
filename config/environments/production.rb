10092f4f85789ea3737a8418bff013ef45b52f0b
EC2 Default User <ec2-user@ip-172-31-81-22.ec2.internal>
2019-11-09 06:46:07 +0000

    updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,21 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "LOCURA_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  
+  
+  
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'aryoma409',
+    password: 'mnsmjvzolrcayygg'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
